da08ad0cfc847ed9ceeacbaf96d7d0f2390b2617
MrSouthWall <147359717+MrSouthWall@users.noreply.github.com>
2024-09-01 17:23:01 +0800

    新增：Add Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com" // 等待 Netlify 配置完成后，在此处输入唯一的 Netlify URL
+  // 等待 Netlify 配置完成后，在此处输入唯一的 Netlify URL
+  site: "https://example.com",
+
+  integrations: [preact()]
 });
